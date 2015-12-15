e6b00e93b1573c8ccc415fa1c4d13538de240f11
Ben Davis <davisben85@gmail.com>
2015-12-15 15:23:14 -0500

    Cleaned up spacing

--- toon/toon.py
+++ toon/toon.py
@@ -6,12 +6,11 @@ class Toon(object,base_hp,base_attack,block_chance):
 		self.attack = base_attack
 		self.block = block_chance
 
-
 	def attack_weight(self,level):
 		attack = self.attack * ( self.level * .25 )
 
-	def defend(self, level):
+	def defend(self,level):
 		defense = self.block * ( self.level * .10 )
 
-	def move(self, speed):
+	def move(self,speed):
 		distance = self.speed
