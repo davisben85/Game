dd91781787e2d235173b91270f880b2ace27db9d
Ben Davis <davisben85@gmail.com>
2015-12-15 16:38:22 -0500

    Created character, added warrior

--- toon/character.py
+++ toon/character.py
@@ -1,5 +1,20 @@
 from toon import Toon
 
-class character(object):
-	def __init__(self):
-		
+class Character(Toon):
+	def __init__(self,name,base_attack,block_chance,base_hp):
+		self.xp = 0
+		super(Character, self).__init__(name,base_attack,block_chance,base_hp)
+
+	def getXP(self):
+		return self.xp
+
+
+##################
+
+
+warrior = Character("Ben",50,20,100)
+
+
+print warrior.getName()
+print warrior.getStats()
+print warrior.getXP()
--- toon/toon.py
+++ toon/toon.py
@@ -2,14 +2,14 @@
 
 class Toon(object):
 
-	def __init__(self,base_hp,base_attack,block_chance,name):
+	def __init__(self,name,base_attack,block_chance,base_hp):
 		self.hp = base_hp
 		self.attack = base_attack
 		self.block = block_chance
 		self.name = name
 
 	def getName(self):
-        return this.name
+		return self.name
 
 	def attack_weight(self,level):
 		attack = self.attack * ( self.level * .25 )
@@ -24,11 +24,18 @@ class Toon(object):
 		current_inventory = self.items
 
 	def takeDamage(self,damage):
-		self.hp - damage
+		self.hp -= damage
 
 	def restoreHealth(self,heal):
 		self.hp += heal
 
 	def isDead(self, hp):
-            if(self.hp <= 0):
-                return True 
+	    if(self.hp <= 0):
+	    	return True
+
+	def getStats(self):
+		stats = []
+		stats.append("Attack:%d" % self.attack)
+		stats.append("Block:%d" % self.block)
+		stats.append("Health:%d" % self.hp)
+		return stats
